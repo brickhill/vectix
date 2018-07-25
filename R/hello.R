6f99e5314d78a19e8087ee3a1a8e940dace76c07
brickhill <petergibson@sbsys.co.uk>
2018-07-25 09:12:48 +0100

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -6,7 +6,7 @@
 #' @param myname your name. Required.
 hello <- function(myname = ""){
   if(myname == ""){
-    stop("Tell me your name!")
+    stop("Tell me your name dude!")
   }
   list(
     message = paste("hello", myname, "! This is", R.Version()$version.string)
