5b7025ba78576822e23b85a3d8048bb930836e96
Peter Gibson <petergibson@sbsys.co.uk>
2018-08-14 22:12:50 +0100

    Include in package

--- R/nob.R
+++ R/nob.R
@@ -1,15 +1,22 @@
+#' Test file
+#' 
+#' Nob
+#' 
+#' @export
+#' @param mydata some object or dataset
+library(ggplot2)
+x <- 1:100
+y <- 1:100
+ddata <- data.frame(x,y)
+
+
 #using ggsave
 ggplot(aes(x, y), data = ddata) +
   geom_point() +
   geom_smooth(method=lm, fill = NA, fullrange=TRUE, color = "black")
-
-ggsave("test.tiff", units="in", width=5, height=4, dpi=300, compression = 'lzw')
-
-#using tiff() and dev.off
-tiff('test.tiff', units="in", width=5, height=4, res=300, compression = 'lzw')
-
-ggplot(aes(x, y), data = ddata) +
-  geom_point() +
-  geom_smooth(method=lm, fill = NA, fullrange=TRUE, color = "black")
-
+x = ggsave("nob.png")
+list(
+  msg = 'test',
+  file = x
+)
 dev.off()
